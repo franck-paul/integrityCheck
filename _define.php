c8dadc3c5b9c7cc6e1d56f47a5f339557c33f860
franck <carnet.franck.paul@gmail.com>
2023-05-15 19:01:55 +0200

    Release 3.2

--- _define.php
+++ _define.php
@@ -13,7 +13,7 @@ $this->registerModule(
     'IntegrityCheck',
     'Checks dotclear installation files integrity',
     'Bruno Hondelatte and contributors',
-    '3.1',
+    '3.2',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
