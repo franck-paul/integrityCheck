88bb4dd8b968890fe42e1e9a7b03d5408ac9e2c4
franck <carnet.franck.paul@gmail.com>
2023-06-15 16:11:57 +0200

    Add void Install.php

--- _define.php
+++ _define.php
@@ -13,7 +13,7 @@ $this->registerModule(
     'IntegrityCheck',
     'Checks dotclear installation files integrity',
     'Bruno Hondelatte and contributors',
-    '3.2',
+    '3.3',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
