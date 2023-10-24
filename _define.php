01750686f3dcc76b85f81a5ffbfd9f9957744b8a
franck <carnet.franck.paul@gmail.com>
2023-10-24 19:28:29 +0200

    New release 5.0

--- _define.php
+++ _define.php
@@ -13,7 +13,7 @@ $this->registerModule(
     'IntegrityCheck',
     'Checks dotclear installation files integrity',
     'Bruno Hondelatte and contributors',
-    '4.0',
+    '5.0',
     [
         'requires' => [['core', '2.28']],
         'type'     => 'plugin',
