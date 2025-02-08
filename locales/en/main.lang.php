35f0103dca907200f72c07827fadbdf147690a11
franck <carnet.franck.paul@gmail.com>
2025-02-08 16:17:59 +0100

    l10n

--- locales/en/main.lang.php
+++ locales/en/main.lang.php
@@ -3,7 +3,6 @@
  * @package Dotclear
  *
  * @copyright Olivier Meunier & Association Dotclear
- * @copyright GPL-2.0-only
  */
 #
 # DOT NOT MODIFY THIS FILE !
@@ -11,9 +10,8 @@
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Integrity Check'] = '';
 L10n::$locales['The following files differ from your initial dotclear installation :'] = '';
 L10n::$locales['An unexpected error occured : '] = '';
 L10n::$locales['System'] = '';
-L10n::$locales['Diagnostics'] = '';
+L10n::$locales['Integrity Check'] = '';
 L10n::$locales['All your installation files are correct.'] = '';
--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -3,7 +3,6 @@
  * @package Dotclear
  *
  * @copyright Olivier Meunier & Association Dotclear
- * @copyright GPL-2.0-only
  */
 #
 # DOT NOT MODIFY THIS FILE !
@@ -11,9 +10,8 @@
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Integrity Check'] = 'Vérification d\'intégrité';
 L10n::$locales['The following files differ from your initial dotclear installation :'] = 'Les fichiers suivants diffèrent de votre installation initiale de dotclear :';
 L10n::$locales['An unexpected error occured : '] = 'Une erreur inattendue s\'est produite : ';
 L10n::$locales['System'] = 'Système';
-L10n::$locales['Diagnostics'] = 'Diagnostics';
+L10n::$locales['Integrity Check'] = 'Vérification d\'intégrité';
 L10n::$locales['All your installation files are correct.'] = 'Tous vos fichiers d\'installation sont corrects.';
